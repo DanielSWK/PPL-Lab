42ba89506e137d715f084d1f86cec9a218be4b12
DanielSWK <151717450+DanielSWK@users.noreply.github.com>
2025-01-23 21:30:54 +0700

    Mengganti zodiak dengan cita-cita

--- introduction.cpp
+++ introduction.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    string nama, jurusan, hobi, zodiak;
+    string nama, jurusan, hobi, citaCita;
     int angkatan, semester;
 
     cout << "Nama Lengkap: ";
@@ -22,15 +22,15 @@ int main() {
     cout << "Hobi: ";
     cin >> hobi;
 
-    cout << "Zodiak :";
-    cin >> zodiak;
+    cout << "Cita-cita :";
+    cin >> citaCita;
     
     cout << "\n--- Template Kalimat Perkenalan Diri ---\n";
     cout << "Halo semua!" << endl;
     cout << "Perkenalkan saya " << nama << " dari program studi " << jurusan << " angkatan " << angkatan << "." << endl;
     cout << "Saat ini saya berada di Semeseter " << semester << "." << endl;
     cout << "Hobi saya adalah " << hobi << "." << endl;
-    cout << "Zodiak saya adalah " << zodiak << "." << endl;
+    cout << "Cita-cita saya adalah " << citaCita << "." << endl;
 
     return 0;
 }
